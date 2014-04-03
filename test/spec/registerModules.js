e360766ab77b4938ae27ec2198da9596599625e5
analog-nico <nicolai.kamenzky@testrails.org>
2014-04-04 01:49:01 +0200

    Refactoring

--- test/spec/registerModules.js
+++ test/spec/registerModules.js
@@ -17,13 +17,13 @@ describe('Regarding module registration, FireUp', function () {
     var filePathToLoad = path.join(__dirname, '../fixtures/modules/simple/simple.js');
     var filePathToIgnore = path.join(__dirname, '../fixtures/modules/simple/notToFireUp.js');
 
-    expect(_.isPlainObject(fireUp._internal.registry.files[filePathToLoad])).toBe(true);
-    expect(_.isPlainObject(fireUp._internal.registry.files[filePathToIgnore])).toBe(true);
+    expect(_.isPlainObject(fireUp._internal.registry.modules[filePathToLoad])).toBe(true);
+    expect(_.isPlainObject(fireUp._internal.registry.modules[filePathToIgnore])).toBe(true);
 
-    expect(fireUp._internal.registry.files[filePathToLoad].status).toBe(fireUp.constants.FILE_STATUS_REGISTERED);
-    expect(fireUp._internal.registry.files[filePathToIgnore].status).toBe(fireUp.constants.FILE_STATUS_TO_IGNORE);
+    expect(fireUp._internal.registry.modules[filePathToLoad].status).toBe(fireUp.constants.FILE_STATUS_REGISTERED);
+    expect(fireUp._internal.registry.modules[filePathToIgnore].status).toBe(fireUp.constants.FILE_STATUS_TO_IGNORE);
 
-    expect(_.isFunction(fireUp._internal.registry.files[filePathToLoad].cache)).toBe(true);
+    expect(_.isFunction(fireUp._internal.registry.modules[filePathToLoad].cache.module)).toBe(true);
 
     done();
 
