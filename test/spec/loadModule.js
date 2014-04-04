3d62f92c57921fb5020f960588940312bee34d96
analog-nico <nicolai.kamenzky@testrails.org>
2014-04-04 22:12:11 +0200

    Loading __module.type

--- test/spec/loadModule.js
+++ test/spec/loadModule.js
@@ -2,6 +2,7 @@
 
 describe('Regarding injection, FireUp', function () {
 
+  var path = require('path');
   var fireUpLib = require('../../lib/index.js');
 
   it('should load modules with direct (not cascading) dependencies', function (done) {
@@ -10,6 +11,11 @@ describe('Regarding injection, FireUp', function () {
       basePath: __dirname,
       modules: ['../fixtures/modules/interfaces/**/*.js', '../fixtures/modules/injection/direct/*.js']
     });
+
+    var moduleFolder = path.relative(process.cwd(), path.join(__dirname, '../fixtures/modules/injection/direct/'));
+    var pathNoDependenciesJs = path.join(moduleFolder, 'noDependencies.js');
+
+    expect(fireUp('injection/direct/noDependencies')).toEqual(pathNoDependenciesJs);
 
     done();
 
