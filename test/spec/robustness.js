3a93e193a95b845173e6b3723af8c75020a771d0
analog-nico <nicolai.kamenzky@testrails.org>
2014-04-18 09:28:14 +0200

    Test for missing implementations

--- test/spec/robustness.js
+++ test/spec/robustness.js
@@ -5,6 +5,55 @@ describe('Regarding its robustness, FireUp', function () {
   var path = require('path');
   var Promise = require('bluebird');
   var fireUpLib = require('../../lib/index.js');
+  var matchers = require('../matchers.js');
+
+
+  beforeEach(function () {
+    this.addMatchers(matchers);
+  });
+
+
+  it('should reject with an NoImplementationError when no implementation error is found', function (done) {
+
+    var fireUp = fireUpLib.newInjector({
+      basePath: __dirname,
+      modules: ['../fixtures/modules/instantiation/failing/*.js']
+    });
+
+    Promise.resolve()
+        .then(function () {
+
+          return fireUp('unknownInterface');
+
+        })
+        .then(function () {
+          done(new Error('fireUp should have rejected the promise.'));
+        })
+        .catch(fireUp.errors.NoImplementationError, function (e) {
+          // This is expected to be called.
+        })
+        .catch(function (e) {
+          done(new Error('fireUp rejected the promise with an error of type ' + e.name + ' (' + e.message + ')'));
+        })
+        .then(function () {
+
+          return fireUp('instantiation/failing/missingDependency');
+
+        })
+        .then(function () {
+          done(new Error('fireUp should have rejected the promise.'));
+        })
+        .catch(fireUp.errors.NoImplementationError, function (e) {
+          // This is expected to be called.
+        })
+        .catch(function (e) {
+          done(new Error('fireUp rejected the promise with an error of type ' + e.name + ' (' + e.message + ')'));
+        })
+        .then(function () {
+          done();
+        });
+
+  });
 
   it('should throw an InstanceInitializationError when creating a module instance fails', function (done) {
 
