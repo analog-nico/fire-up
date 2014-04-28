7d0324fa28e0eed789140dabf8b2652278e2de9a
analog-nico <nicolai.kamenzky@testrails.org>
2014-04-28 09:18:36 +0200

    Tests for 'promise' (npm package) promises

---
+++ test/fixtures/modules/instantiation/promises/promise.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Fire me up!
+
+module.exports = function(Promise, mode, id) {
+
+  return new Promise(function (resolve, reject) {
+
+    if (mode === 1) {
+      throw new Error(require('path').relative(process.cwd(), __filename) + '_' + id);
+    }
+
+    setTimeout(function () {
+      if (mode === 0) {
+        resolve(require('path').relative(process.cwd(), __filename) + '_' + id);
+      } else if (mode === 2) {
+        reject(require('path').relative(process.cwd(), __filename) + '_' + id);
+      }
+    }, 10);
+
+  });
+
+};
+
+module.exports.__module = {
+  implements: 'instantiation/promises/promise',
+  inject: 'require(promise)',
+  type: 'multiple instances'
+};
--- test/spec/promises.js
+++ test/spec/promises.js
@@ -66,6 +66,65 @@ describe('Regarding promises, FireUp', function () {
 
   });
 
+  it('should handle promise promises', function (done) {
+
+    var fireUp = fireUpLib.newInjector({
+      basePath: __dirname,
+      modules: ['../fixtures/modules/instantiation/promises/*.js'],
+      require: require
+    });
+
+    var folder = path.relative(process.cwd(), path.join(__dirname, '../fixtures/modules/instantiation/promises/'));
+
+    Promise.resolve()
+        .then(function () {
+
+          return fireUp('instantiation/promises/promise(0,1)');
+
+        })
+        .then(function (instance) {
+          expect(instance).toEqual(path.join(folder, 'promise.js_1'));
+        })
+        .catch(function (e) {
+          done(new Error('promise(1) should not have thrown an error.'));
+        })
+        .then(function () {
+
+          return fireUp('instantiation/promises/promise(1,2)');
+
+        })
+        .then(function (instance) {
+          done(new Error('promise(1) should have thrown an error.'));
+        })
+        .catch(fireUp.errors.InstanceInitializationError, function (e) {
+          expect(e.cause.message).toEqual(path.join(folder, 'promise.js_2'));
+        })
+        .catch(function (e) {
+          done(new Error('fireUp rejected the promise with an error of type ' + e.name + ' (' + e.message + ')'));
+        })
+        .then(function () {
+
+          return fireUp('instantiation/promises/promise(2,3)');
+
+        })
+        .then(function (instance) {
+          done(new Error('promise(2) should have thrown an error.'));
+        })
+        .catch(fireUp.errors.InstanceInitializationError, function (e) {
+          expect(e.cause).toEqual(path.join(folder, 'promise.js_3'));
+        })
+        .catch(function (e) {
+          done(new Error('fireUp rejected the promise with an error of type ' + e.name + ' (' + e.message + ')'));
+        })
+        .then(function () {
+          done();
+        })
+        .catch(function (e) {
+          done(e);
+        });
+
+  });
+
   it('should handle RSVP promises', function (done) {
 
     var fireUp = fireUpLib.newInjector({
